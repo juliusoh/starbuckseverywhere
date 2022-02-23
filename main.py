25fb93f5a5473b35c90b3829b87844090d89bad2
Julius Oh <juliusoh@gmail.com>
2022-02-22 18:10:16 -0800

    hah

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import requests
 import pandas as pd
-
-url = "http://www.starbuckseverywhere.net/StoreClosingDates.htm"
-
-page = requests.get(url)
-
-df = pd.read_html(url)[0]
-# df.columns = ["Closed", "Name", "City", "Market"]
-headers = df.iloc[0]
-df = df[1:]
-df.columns = headers
-df.to_csv('data.csv', index= False)
-print(df)
 
 # starbucks_table = soup.findChildren('table')[0]
 
@@ -22,3 +9,14 @@ print(df)
 #     for cell in cells:
 #         cellText = cell.text
 
+def main():
+  url = "http://www.starbuckseverywhere.net/StoreClosingDates.htm"
+  df = pd.read_html(url)[0]
+  # df.columns = ["Closed", "Name", "City", "Market"]
+  headers = df.iloc[0]
+  df = df[1:]
+  df.columns = headers
+  df.to_csv('data.csv', index= False)
+  print(df)
+
+main()
